bf8b00e3fc7276469508d3401a8b33701aa74286
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-02-24 14:29:11 -0300

    feature: Apply pattern facade to  account deposit and withdraw

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,9 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
+  alias Rocketpay.Accounts.{Deposit, WithDraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
+  defdelegate withdraw(params), to: WithDraw, as: :call
 end
