db26ddaa3f7aead813437e11591ff865820c7a5f
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-02-24 14:30:14 -0300

    feature: Added accounts router to deposit and withdraw

---
+++ lib/rocketpay/accounts/withdraw.ex
@@ -0,0 +1,4 @@
+defmodule Rocketpay.Accounts.WithDraw do
+  def call(params) do
+  end
+end
---
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,21 @@
+defmodule RocketpayWeb.AccountsController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Account
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", %{account: account})
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", %{account: account})
+    end
+  end
+end
