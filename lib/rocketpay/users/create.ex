a88fecb6b6eda909d4326531fa1d41e5eaeee8e8
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-02-24 11:08:23 -0300

    refactor: Create user and account by transaction, add association has one in user and adjust user_view to return account

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,9 +1,35 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{Repo, User}
+  alias Ecto.Multi
+  alias Rocketpay.{Account, Repo, User}
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(user, repo)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(repo, user)
+    end)
+    |> run_trasaction()
+  end
+
+  defp insert_account(user, repo) do
+    user.id
+    |> account_chageset()
+    |> repo.insert()
+  end
+
+  defp account_chageset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
+
+  defp preload_data(repo, %User{} = user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_trasaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _failed_operation, reason, _changes_so_far} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
--- lib/rocketpay_web/views/users_view.ex
+++ lib/rocketpay_web/views/users_view.ex
@@ -1,14 +1,25 @@
 defmodule RocketpayWeb.UsersView do
   use RocketpayWeb, :view
-  alias Rocketpay.User
+  alias Rocketpay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          account: %Account{id: account_id, balance: balance}
+        }
+      }) do
     %{
       message: "user created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
