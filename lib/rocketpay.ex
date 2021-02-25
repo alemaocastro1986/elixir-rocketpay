76815e1832f7a5d3f6c2d1ed483b44572577c629
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2021-02-25 09:54:15 -0300

    refactor: Adjust name module

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,9 +1,9 @@
 defmodule Rocketpay do
   alias Rocketpay.Users.Create, as: UserCreate
-  alias Rocketpay.Accounts.{Deposit, WithDraw}
+  alias Rocketpay.Accounts.{Deposit, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
-  defdelegate withdraw(params), to: WithDraw, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
